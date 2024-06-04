72661c0210e6e9c55b46e0340baefda0fb84cd1d
foreverfl <forever_fl@naver.com>
2024-06-05 01:12:28 +0900

    Add Item54, Item55

--- src/chapter08/Item54_ReturnEmptyCollectionsOrArraysNotNulls.java
+++ src/chapter08/Item54_ReturnEmptyCollectionsOrArraysNotNulls.java
@@ -1,5 +1,36 @@
 package chapter08;
 
-public class Item54_ReturnEmptyCollectionsOrArraysNotNulls {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Item54_ReturnEmptyCollectionsOrArraysNotNulls {
+    public static void main(String[] args) {
+        List<String> list1 = getList(true);
+        System.out.println("list1: " + list1);
+
+        List<String> list2 = getList(false);
+        System.out.println("list2: " + list2);
+
+        // null 체크 없이 바로 사용 가능
+        for (String item : list1) {
+            System.out.println("item: " + item);
+        }
+
+        // null 체크 없이 바로 사용 가능
+        for (String item : list2) {
+            System.out.println("item: " + item);
+        }
+    }
+
+    public static List<String> getList(boolean isEmpty) {
+        if (isEmpty) {
+            return Collections.emptyList();
+        } else {
+            List<String> list = new ArrayList<>();
+            list.add("apple");
+            list.add("banana");
+            return list;
+        }
+    }
 }
--- src/chapter08/Item55_ReturnOptionalsJudiciously.java
+++ src/chapter08/Item55_ReturnOptionalsJudiciously.java
@@ -1,5 +1,34 @@
 package chapter08;
 
+import java.util.Optional;
+
 public class Item55_ReturnOptionalsJudiciously {
+
+    public static Optional<String> getOptionalValue(String email) {
+        return Optional.ofNullable(email);
+    }
+
+    public static void main(String[] args) {
+        // 기본값 정하기
+        String value1 = getOptionalValue("john@example.com").orElse("No email provided");
+        System.out.println("Value 1: " + value1);
+
+        // 원하는 예외 던지기
+        // String value2 =
+        // getOptionalValue(null).orElseThrow(IllegalArgumentException::new);
+        // System.out.println("Value 2: " + value2);
+
+        // 항상 값이 채워져 있다고 가정하기
+        Optional<String> optionalValue = getOptionalValue("jane@example.com");
+        if (optionalValue.isPresent()) {
+            String email = optionalValue.get();
+            System.out.println("Value 3: " + email);
+        }
+
+        // Supplier를 활용한 초기 설정 비용 낮추기
+        String value4 = getOptionalValue(null).orElseGet(() -> "Default email");// Optional 객체가 비어있을 때만 기본값을 생성하는 로직이므로
+                                                                                // 초기 설정 비용이 낮아짐
+        System.out.println("Value 4: " + value4);
+    }
 
 }
